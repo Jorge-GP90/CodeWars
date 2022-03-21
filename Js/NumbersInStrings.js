1ecf37a98e95057a17cf2cfa93a8c5109be39da9
George Garcia <jorge.90gap@gmail.com>
2022-03-21 18:42:20 +0900

    NumberIn Strings and Powers of 2 solved

--- Js/NumbersInStrings.js
+++ Js/NumbersInStrings.js
@@ -6,7 +6,36 @@ For example, solve("gh12cdy695m1") = 695, because this is the largest of all num
 
 */
 
+
+
+// first way with regex
 function solve(s){
-        
-    
-};
+    let arr = s.replace(/[^0-9/]/g, ' ').split(' ')
+    return Math.max(...arr)
+  }
+ 
+  console.log(solve("gh12cdy695m1"));
+
+  
+//   second way with map, parseInt and NaN
+
+function solve(s){
+    return s.split('').map(item => parseInt(item)).reduce((sum, item, i, arr) => !isNaN(item) ? sum + item : isNaN(item) && !isNaN(arr[i-1]) ? sum+' ' : sum+'','').split(' ').map(item => parseInt(item)).filter(item => !isNaN(item)).sort((a, b) => b-a)[0];
+  };
+
+
+console.log(solve("gh12ce4666dy695m1"));
+
+// best practice solution in function statement
+
+  function solve(s){
+    return Math.max(...s.match(/\d+/g));
+  };
+ 
+console.log(solve("gh1299cdy695m1"));
+//   best practice solution with arrow
+
+solve=s=>Math.max(...s.match(/\d+|$/g));
+
+
+console.log(solve("gh12cdy695m1099"));
--- Js/PowersOf2.js
+++ Js/PowersOf2.js
@@ -10,6 +10,64 @@ n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
 */
 
 
+/*
+n = 2;  
+
 function powersOfTwo(n){
-  return [];
+  let i = 1;
+  while (i < n){
+    i *= 2;
+  }
+  return i === n;
+  console.log(i === n);
 }
+*/
+
+// display the exponential of the number of n in array  ( but not previews ones) 
+function powersOfTwo(n) {
+  let pwrTwo = [];
+  for (let i = 0; i < n; i++) {
+    pwrTwo[i] = Math.pow(n,2);
+  }
+  return pwrTwo;
+}
+console.log(powersOfTwo(2));
+
+// pushing the number of n in array ( but no the exponent)
+function pwOfTwo(n) {
+  let pwrTw = [];
+  for (let i = 0; i < n; i++) {
+    num = Math.pow(n,2);
+    pwrTw[i] = pwrTw.push(num);
+
+  }
+  return pwrTw;
+}
+console.log(pwOfTwo(5));
+
+// first way correct
+
+function poTwo(n) {
+  let pwTw = [];
+  for (let i = 0; i <= n; i++) {
+    pwTw.push(Math.pow(2,i)); 
+  }
+  return pwTw;
+}
+console.log(poTwo(4));
+
+// Second way correct
+function powersOfTwo(n){
+  let res = [];
+  do {
+    res.unshift(Math.pow(2, n));
+  } while (n--);
+  return res;
+}
+console.log(poTwo(5));
+
+// third way correct using map
+function powersOfTwo(n) {
+  return [...Array(n + 1)].map((_, i) => 2 ** i)
+}
+console.log(poTwo(6));
