a13edf1c1c1f9b95247a9112504efb3b244c94de
George Garcia <jorge.90gap@gmail.com>
2022-03-25 00:08:46 +0900

    multiplying two numbers  arrow function

--- Js/multiplyingTwoNumbers.js
+++ Js/multiplyingTwoNumbers.js
@@ -2,6 +2,18 @@
 
 // implement a fucntion that multiplying two numbers
 
-const = multiply (value1, value2) => value1*value2;
 
-console.log(multiply(2 ,3));	
+
+const multiply = (value1, value2) =>  value1*value2 
+
+console.log(multiply(2 ,3));
+
+
+//second way (best practices?)
+
+function multiply2(a, b){
+  if(typeof a == 'number' && typeof b == 'number')
+    return a * b;  
+}	
+
+console.log(multiply2(2 ,3));
