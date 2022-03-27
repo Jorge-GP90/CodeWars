81449e140b7917f8d6c9c75d5df56268f6f8b04f
George Garcia <jorge.90gap@gmail.com>
2022-03-27 14:48:55 +0900

    Double The Integer solved

--- Js/DoubleTheInteger.js
+++ Js/DoubleTheInteger.js
@@ -1,5 +1,20 @@
 // Code as fast as you can! You need to double the integer and return it.
 
 function doubleInteger(i) {
-  return i ** 2;
+  return i * 2;
+}
+
+// Second way (arrow functions)
+
+const doubleInteger = (i) => 2 * i;
+
+// third way
+
+function doubleInteger(i) {
+  return i + i;
+}
+
+// Fourth way
+function doubleInteger(i) {
+  return i << 1;
 }
