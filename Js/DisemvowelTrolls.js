cbaa6cb9ea597db7861796845e373301559f106c
George Garcia <jorge.90gap@gmail.com>
2022-04-03 15:01:01 +0900

    Disem vowels Trolls solved

--- Js/DisemvowelTrolls.js
+++ Js/DisemvowelTrolls.js
@@ -12,4 +12,52 @@ Note: for this kata y isn't considered a vowel.
 
 */
 
-const disemvowel = (str) => {};
+// first way to solve
+
+const disemvowel = (str) => {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str.toLowerCase()[i] === "a" ||
+      str.toLowerCase()[i] === "e" ||
+      str.toLowerCase()[i] === "i" ||
+      str.toLowerCase()[i] === "o" ||
+      str.toLowerCase()[i] === "u"
+    ) {
+      continue;
+    }
+    result += str[i];
+  }
+  return result;
+};
+console.log(disemvowel("This website is for losers LOL!"));
+
+// second way (Shortest)
+
+function disemvowel(str) {
+  return str.replace(/[aeiou]/gi, "");
+}
+
+// third way
+
+const vowels = "aeiou";
+
+function disemvowel(str) {
+  return str
+    .split("")
+    .filter((letter) => !vowels.includes(letter.toLowerCase()))
+    .join("");
+}
+
+// fourth way
+
+function disemvowel(str) {
+  var vowels = ["a", "e", "i", "o", "u"];
+
+  return str
+    .split("")
+    .filter(function (el) {
+      return vowels.indexOf(el.toLowerCase()) == -1;
+    })
+    .join("");
+}
