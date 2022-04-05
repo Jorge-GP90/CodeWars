d97b8c782f091cbcdf43245ef652534b41a077cd
George Garcia <jorge.90gap@gmail.com>
2022-04-05 23:14:35 +0900

    Convert String to Array solved

--- Js/ConvertStringToArray.js
+++ Js/ConvertStringToArray.js
@@ -17,6 +17,48 @@ Examples (Input -> Output):
 
 */
 
-function stringToArray(string) {
-  // code code code
+const stringToArray1 = (string) => {
+  let result = [];
+
+  let strSplit = string.split("");
+  for (let i = 0; i < strSplit.length; i++) {
+    if (strSplit[i] === "") {
+      result[i].push();
+    }
+    strSplit.join();
+  }
+
+  return result[i];
+};
+
+// First solution
+const stringToArray = (string) => string.split(/[" "]/);
+
+console.log(stringToArray("hello I could make it"));
+
+// Second Way
+
+const stringToArray2 = (string) => {
+  return string.trim().split(" ");
+};
+
+console.log(stringToArray2("hello I could make it twice"));
+
+// Other way
+
+function stringToArray3(string) {
+  var result = [];
+  var traverse = function foo(string) {
+    for (var i = 0; i < string.length; i++) {
+      var chr = string.charAt(i);
+      if (chr === " ") {
+        result.push(string.slice(0, i));
+        foo(string.slice(i + 1));
+        return;
+      }
+    }
+    result.push(string);
+  };
+  traverse(string);
+  return result;
 }
