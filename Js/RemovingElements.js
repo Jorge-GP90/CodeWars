28cf4cb3969ae43d633f621144ce9665c5af4a97
George Garcia <jorge.90gap@gmail.com>
2022-05-03 22:35:20 +0900

    Removing Elements solved

--- Js/RemovingElements.js
+++ Js/RemovingElements.js
@@ -10,5 +10,28 @@ None of the arrays will be empty, so you don't have to worry about that!
 */
 
 const removeEveryOther = (arr) => {
-  //your code here
+  return arr.filter((_, i) => i % 2 == 0);
 };
+
+console.log(removeEveryOther(["Keep", "Remove", "Keep", "Remove", "Keep"]));
+
+// second solution
+function removeEveryOther2(arr) {
+  var newArr = [];
+  for (var i = 0; i < arr.length; i += 2) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
+}
+
+// third solution
+function removeEveryOther3(arr) {
+  for (var i = 1; i < arr.length; i++) {
+    arr.splice(i, 1);
+  }
+  return arr;
+}
+
+// Fourth solution
+
+const removeEveryOther4 = (arr) => arr.filter((_, i) => !(i % 2));
