a5f00584f4c7b02766a202952f7308c6285a565b
George Garcia <jorge.90gap@gmail.com>
2022-05-15 23:03:10 +0900

    Define Cards Suit solved

--- Js/DefineCardSuit.js
+++ Js/DefineCardSuit.js
@@ -14,5 +14,45 @@ Our deck (is preloaded):
 */
 
 function defineSuit(card) {
-  // good luck
+  const value = [...card];
+  const diamonds = "♦";
+  const clubs = "♣";
+  const hearts = "♥";
+  const spades = "♠";
+  if (value.find((d) => d === diamonds)) return "diamonds";
+  if (value.find((c) => c === clubs)) return "clubs";
+  if (value.find((h) => h === hearts)) return "hearts";
+  if (value.find((s) => s === spades)) return "spades";
+}
+
+console.log(defineSuit("Q♠"));
+
+// Second solution
+
+function defineSuit2(card) {
+  const s = {
+    "♣": "clubs",
+    "♠": "spades",
+    "♦": "diamonds",
+    "♥": "hearts",
+  };
+  return s[card.charAt(card.length - 1)];
+}
+
+// third solution
+function defineSuit3(card) {
+  return {
+    "♣": "clubs",
+    "♦": "diamonds",
+    "♥": "hearts",
+    "♠": "spades",
+  }[card.substr(-1)];
+}
+
+// fouth solution
+function defineSuit4(card) {
+  if (card.includes("♥")) return "hearts";
+  if (card.includes("♦")) return "diamonds";
+  if (card.includes("♣")) return "clubs";
+  if (card.includes("♠")) return "spades";
 }
