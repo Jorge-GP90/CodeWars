43c6d7da115d868efdce9e6c9f0e18fb8d4fb18d
George Garcia <jorge.90gap@gmail.com>
2022-05-29 23:20:19 +0900

    Is Palindrome SOLVED

--- Js/IsAPalindrome.js
+++ Js/IsAPalindrome.js
@@ -2,6 +2,41 @@
 Write a function that checks if a given string (case insensitive) is a palindrome.
 */
 
+function isPalindromeABC(x) {
+  const value = x.toLowerCase().split("");
+  const half = value.length / 2;
+  let arrOne = value.slice(0, half);
+  let arrTwo = value.slice(half, value.length);
+  const compare = arrOne.reverse();
+  const res = `${compare}` === `${arrTwo}`;
+  return res;
+}
+
 function isPalindrome(x) {
-  // your code here
+  const value = x.toLowerCase().split("");
+  return `${value}` === `${value.reverse()}` ? true : false;
+}
+
+console.log(isPalindrome("hello"));
+
+// Second solution
+
+const isPalindrome2 = (x) => {
+  return x.split("").reverse().join("").toLowerCase() === x.toLowerCase()
+    ? true
+    : false;
+};
+
+// third solution
+
+function isPalindrome3(x) {
+  x = x.toLowerCase();
+  let a = 0;
+  let b = x.length;
+  while (a < b) {
+    if (x[a] != x[b - 1]) return false;
+    a++;
+    b--;
+  }
+  return true;
 }
