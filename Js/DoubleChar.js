aa00bd83098529043e4dc7987a0355baef6c8b9e
George Garcia <jorge.90gap@gmail.com>
2022-05-31 13:10:49 +0900

    Double Chart Solved

--- Js/DoubleChar.js
+++ Js/DoubleChar.js
@@ -10,5 +10,31 @@ Good Luck!
 */
 
 function doubleChar(str) {
-  // Your code here
+  const double = [...str].map((x) => x + x);
+  return `${double.join("")}`;
+}
+
+console.log(doubleChar("String"));
+
+// second solution
+
+const doubleChar2 = (str) =>
+  str
+    .split("")
+    .map((c) => c + c)
+    .join("");
+
+// third solution
+
+function doubleChar(str) {
+  return str.replace(/(.)/g, "$1$1");
+}
+
+// fourth solution
+function doubleChar(str) {
+  var word = "";
+  for (var i = 0; i < str.length; i++) {
+    word = word + str[i] + str[i];
+  }
+  return word;
 }
